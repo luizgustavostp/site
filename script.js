204614903301f468050c86c3b9d3988bce535523
Luiz Gustavo <luizgustavosantospreira@gmail.com>
2026-01-19 00:43:17 -0300

    update

--- script.js
+++ script.js
@@ -54,7 +54,13 @@ function trocarImagem() {
     lataffa.src = "imgs/lataffa-banner(satured.png";
     lataffa2.src = "imgs/lataffa-banner(satured.png";
   }
-
+    document.body.style.zoom = "90%"
+    if (window.innerWidth <= 1700) {
+    document.body.style.zoom = "100%"
+    }
+    else {
+        document.body.style.zoom = "90%"
+    }
 }
 
 
