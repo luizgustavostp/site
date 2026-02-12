6682b0036913132cc8dcb195104b854ddecf6029
Luiz Gustavo <luizgustavosantospreira@gmail.com>
2026-02-12 05:17:08 -0300

    update

---
+++ script/carrinho.js
@@ -0,0 +1,68 @@
+const cart = JSON.parse(localStorage.getItem("cart"))
+
+async function loadcarrinho() {
+    let tot = 0
+    let productinfo = JSON.parse(localStorage.getItem("catalog"))
+    if (!productinfo) {
+        const response = await fetch("http://localhost/site/produtos.php");
+        const data = await response.json();
+        productinfo = data
+    }
+    cart.forEach(element => {
+        let item = productinfo.find(item => item.id == element.id) 
+        let newproductcart = document.getElementsByClassName("product-carrinho")[0].cloneNode(true)
+        let productC = newproductcart.children
+        let texts = productC[1].children
+        let nameinput = texts[0]
+        let priceinput = texts[1]
+        let qntd = element.quantidade
+        let quantidadediv = productC[2].children
+        productC[0].src = item.imagepath
+        nameinput.innerText = item.name
+        priceinput.innerText = "R$"
+        priceinput.innerText += item.price
+        quantidadediv[1].innerText = element.quantidade
+        let priceN = Number(item.price)
+        function qnt(bool) {
+            if (bool && qntd < 10 && qntd >= 0) {
+                qntd++
+                tot += priceN
+            }
+            else if (qntd == 1 && !bool) {
+                apagar(element,cart)
+            }
+            else if (!bool && qntd > 0) {
+                qntd--
+                tot -= priceN
+            }
+            quantidadediv[1].innerText = qntd
+            document.getElementById("pricetotal").textContent = `R$${tot}`
+            element.quantidade = qntd
+            localStorage.setItem("cart",JSON.stringify(cart))
+        }
+        quantidadediv[0].addEventListener("click",() => {
+            qnt(false)
+        })
+        quantidadediv[2].addEventListener("click",() => {
+            qnt(true)
+        })
+        productC[3].addEventListener("click",() => {
+            apagar(element,cart)
+        })
+
+        newproductcart.style.display = "flex"
+        document.getElementById("carrinho").appendChild(newproductcart)
+
+        tot += (priceN * qntd)
+        document.getElementById("pricetotal").textContent = `R$${tot}`
+    });
+}
+loadcarrinho()
+function apagar(element,cart) {
+    const index = cart.findIndex(item => item.id == element.id);
+    if (index !== -1) {
+        cart.splice(index, 1);
+    }
+    localStorage.setItem("cart",JSON.stringify(cart))
+    window.location.reload()
+}
--- script/item.js
+++ script/item.js
@@ -1,3 +1,6 @@
+let price
+let id
+const sucessAdded = document.getElementById("Adicionado-sucesso")
 function start() {
     const params = new URLSearchParams(window.location.search)
     const termo = params.get("id")
@@ -12,9 +15,99 @@ function start() {
     document.getElementById("tittle-product").textContent = item.name
     document.getElementById("price-product").textContent = "R$ "
     document.getElementById("price-product").textContent += item.price
+    price = item.price
+    id = termo
     document.getElementById("price-product").textContent.replace(".",",")
-    document.getElementById("stars").src = item.starspath
+
     console.log(item.imagepath,item.imagepath2)
 }
 start()
+const imgs = document.getElementById("imgs")
+let qt = 5
+document.getElementById("left").addEventListener("click",() => {
+    imgs.style.transform = `translateX(-0%)`
+})
+document.getElementById("right").addEventListener("click",() => {
+    imgs.style.transform = `translateX(-${qt}0%)`
+})
 
+
+const formasdisplayC = document.getElementById("formas-display").children
+const arrry = [...formasdisplayC]
+console.log(arrry)
+arrry.forEach(element => {
+    element.addEventListener("click",() => {
+        arrry.forEach(elements => {
+            elements.removeAttribute("class","method")
+        })
+        element.setAttribute("class","method")
+    })
+})
+
+const menos = document.getElementById("quantidade-menos")
+const mais = document.getElementById("quantidade-mais")
+const text = document.getElementById("quantidade-text")
+let quantidade = 1
+
+console.log(price)
+menos.addEventListener("click",() => {
+    quantidade--
+    text.textContent = quantidade
+    document.getElementById("price-product").textContent = "R$ "
+    document.getElementById("price-product").textContent += price * quantidade
+})
+
+mais.addEventListener("click",() => {
+    quantidade++
+    text.textContent = quantidade
+    document.getElementById("price-product").textContent = "R$ "
+    document.getElementById("price-product").textContent += price * quantidade
+})
+
+
+function ld() {
+    if (window.innerWidth < 482) {
+        qt = 10
+            console.log(qt)
+    }
+    else {
+        qt = 5
+    }
+
+}
+window.addEventListener("load",() => {
+    ld()
+})
+window.addEventListener("resize",() => {
+    ld()    
+})
+
+const carrinhobtn = document.getElementById("adicionar-carrinho")
+carrinhobtn.addEventListener("click",() => {
+    let carrinho = JSON.parse(localStorage.getItem("cart"))
+    console.log(carrinho)
+    if (carrinho) {
+        carrinho.push({
+            quantidade,id
+        })
+        console.log("tem carrinho patrao")
+    }
+    else {
+        carrinho = [{
+            quantidade,id
+        }]
+        console.log("nao tem nao cria")
+    }
+    localStorage.removeItem("cart")
+    localStorage.setItem("cart",JSON.stringify(carrinho))
+    document.getElementById("blur").style.display = "block"
+    document.getElementById("blur").style.visibility = "visible"
+    sucessAdded.style.display = "block"
+    console.log("fudido")
+})
+const okbtn = document.getElementById("ok")
+okbtn.addEventListener("click",() => {
+    document.getElementById("blur").style.display = "none"
+    document.getElementById("blur").style.visibility = "hidden"
+    sucessAdded.style.display = "none"
+})
